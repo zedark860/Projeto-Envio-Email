534a8f9fc7b1eb6ea1b1930a464e686b335a90c3
Davi <oversouls11@gmail.com>
2024-09-20 00:11:11 -0300

    Arrumando pastas e última versão atual
    Arrumando algumas partes do projeto;
    Implementado randomização de tempo no envio;
    Implementado carregamento do html do email;
    Melhorias substânciais no código;

---
+++ Versão Recente/src/model/SenderEmailModel.py
@@ -0,0 +1,17 @@
+from src.controller.DataUserController import DataUserController
+from pydantic import BaseModel, Field
+
+class SenderEmailModel(BaseModel):
+    data_user: DataUserController
+    spreadsheet_path: str = Field(..., description="Caminho da planilha")
+    html_path: str = Field(..., description="Caminho do HTML")
+    email_send_quantity: int = Field(..., description="Quantidade de envio de email")
+    send_interval: int = Field(..., description="Intervalo de envio")
+    email_subject: str = Field(..., description="Assunto do email")
+    email_title: str = Field(..., description="Título do email")
+    email_message: str = Field(..., description="Mensagem do email")
+    whatsapp_redirect_number: str = Field(..., description="Número do whatsapp para redirecionamento")
+    
+    class Config:
+        from_attributes = True
+    
